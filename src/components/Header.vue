faf253b9a7720ab2b4c2af30a139647c4ec263c8
John Kennedy <devjckennedy@gmail.com>
2018-04-09 09:02:17 +0100

    implement basic header component

---
+++ src/components/Header.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-toolbar>
+    <v-toolbar-title>Top Headlines</v-toolbar-title>
+    <v-container>
+      <v-layout row wrap>
+        <v-flex xs6>
+          <v-select
+            :items="items"
+            v-model="newsSource"
+            label="News Sources"
+            single-line
+            append-icon="fa-newspaper"
+            auto-complete
+            search-input.sync
+          ></v-select>
+        </v-flex>
+        <v-spacer></v-spacer>
+        <v-flex xs4>
+          <v-select
+            :items="items"
+            v-model="language"
+            label="Language"
+            single-line
+            append-icon="map"
+            auto-complete
+            search-input.sync
+          ></v-select>
+        </v-flex>
+      </v-layout>
+    </v-container>
+    <v-spacer></v-spacer>
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn flat>Link One</v-btn>
+      <v-btn flat>Link Two</v-btn>
+      <v-btn flat>Link Three</v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [],
+      newsSource: '',
+      language: ''
+    };
+  },
+  methods: {}
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/NewsSource.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-content>
+    <v-container fluid>
+      News Sources Cards Go here
+    </v-container>
+  </v-content>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
