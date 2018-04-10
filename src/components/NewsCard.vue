842350fcf0d79f8eecdc11d253be0537c53855ef
John Kennedy <devjckennedy@gmail.com>
2018-04-10 22:50:22 +0100

    fix error where title or description can be null

--- src/components/NewsCard.vue
+++ src/components/NewsCard.vue
@@ -1,11 +1,11 @@
 <template>
-  <v-card height="501px">
+  <v-card height="400px">
     <v-card-media :src="article.urlToImage" height="200px">
     </v-card-media>
     <v-card-title primary-title>
       <div>
-        <h3 class="headline mb-0">{{ article.title }}</h3>
-        <div>{{ article.description.substr(0, 150) }} ...</div>
+        <h3 class="headline mb-0">{{ getTitle(article.title) }}</h3>
+        <div>{{ getDescription(article.description) }}</div>
       </div>
     </v-card-title>
     <v-card-actions>
@@ -16,7 +16,21 @@
 
 <script>
 export default {
-  props: ['article']
+  props: ['article'],
+  methods: {
+    getTitle(text) {
+      if (!text) {
+        return 'No title found for this article'
+      }
+      return `${text.substr(0, 50)}...`
+    },
+    getDescription(text) {
+       if (!text) {
+        return 'No description found for this article'
+      }
+      return `${text.substr(0, 100)}...`
+    }
+  }
 }
 </script>
 
