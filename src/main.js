b37b1ed1abaccc6a245c80754012c7194575812e
John Kennedy <devjckennedy@gmail.com>
2018-04-10 10:24:33 +0100

    create event bus for emiting data

--- src/main.js
+++ src/main.js
@@ -1,11 +1,21 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
+import VueResource from 'vue-resource'
 
 import App from './App.vue'
 
 import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Vuetify)
+Vue.use(VueResource)
+
+export const EVENT_BUS = new Vue({
+  methods: {
+    setArticles(articles) {
+      this.$emit('articlesSet', articles)
+    }
+  }
+})
 
 new Vue({
   el: '#app',
