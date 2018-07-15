b05b70c11e475029f03183acecf4e8b2405fc945
John Kennedy <devjckennedy@gmail.com>
2018-07-15 11:25:08 +0100

    change username

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
   <v-footer class="pa-3" fixed>
     <v-spacer></v-spacer>
     <div class="text-xs-center">
-      Made with <v-icon>fas fa-heart</v-icon> by <v-btn href="https://github.com/weboluchi" target="_blank" flat color="info" dark>Kodedly</v-btn>
+      Made with <v-icon>fas fa-heart</v-icon> by <v-btn href="https://github.com/codejockie" target="_blank" flat color="info" dark>codejockie</v-btn>
     </div>
     <div>&copy; {{ new Date().getFullYear() }}</div>
   </v-footer>
